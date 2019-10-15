2a2e863d601c78e6b0c13edc5686b5263562604f
morpheus <mariageorgepapas@gmail.com>
2019-10-15 16:50:58 -0500

    final addition of templates

--- main.py
+++ main.py
@@ -6,20 +6,16 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
-# TO DO fix the form with the render_template method
-
 @app.route("/", methods=['POST'])
 def encrypt():
     user_sentence = str(request.form["text"])
     user_rotation = int(request.form["rot"])
     encrypted_message = rotate_string(user_sentence, user_rotation)
     return render_template('form.html', encrypted_message=encrypted_message)
-     
 
 @app.route("/")
 def index():
-    return render_template('form.html') #form.format("")
+    return render_template('form.html') 
 
 @app.route("/encrypted-message")
 def encryption():    
@@ -27,6 +23,3 @@ def encryption():
     return render_template('form.html', encrypted_message=encrypted_message)
 
 app.run()
-
-#  username = request.args.get('username')
-#     return render_template('welcome_form.html', username=username)
