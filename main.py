f4fd19ab41f6f7079b02a306ad4ac888fe9dcfe4
morpheus <mariageorgepapas@gmail.com>
2019-10-15 16:41:47 -0500

    added templates

--- main.py
+++ main.py
@@ -1,51 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from caesar import rotate_string
+import cgi
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!DOCTYPE html>
 
-<html>
-    <head>
-        <form method="post">
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-        Rotate by: 
-        <input type="text" name="rot" value="0">
-        <textarea rows="4" cols="50" name="text">{0}</textarea>
-        <input type="submit" value="Submit">
-    </head>
-    <body>
-      <!-- create your form here -->
-    </body>
-</html>
-"""
+# TO DO fix the form with the render_template method
 
 @app.route("/", methods=['POST'])
 def encrypt():
     user_sentence = str(request.form["text"])
     user_rotation = int(request.form["rot"])
     encrypted_message = rotate_string(user_sentence, user_rotation)
-    return form.format(encrypted_message)
+    return render_template('form.html', encrypted_message=encrypted_message)
      
 
 @app.route("/")
 def index():
-    return form.format("")
+    return render_template('form.html') #form.format("")
+
+@app.route("/encrypted-message")
+def encryption():    
+    encrypted_message = request.args.get('user_sentence', 'user_rotation')
+    return render_template('form.html', encrypted_message=encrypted_message)
 
 app.run()
+
+#  username = request.args.get('username')
+#     return render_template('welcome_form.html', username=username)
